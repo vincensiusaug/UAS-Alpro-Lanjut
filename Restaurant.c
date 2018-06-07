ca509dac8213fdf33195595b69d0ba9f09552559
Vincensius Augustino <39743055+vincensiusaug@users.noreply.github.com>
2018-06-07 18:41:11 +0700

    Template

---
+++ Restaurant.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+void File_Read(){
+
+}
+
+void File_Write(){
+
+}
+
+void Data_Show(){
+
+}
+
+void Data_Hide(){
+
+}
+
+void Data_Unhide(){
+
+}
+
+void Data_Add(){
+
+}
+
+void Data_Sort(){
+
+}
+
+void Main_Menu(){
+    
+}
+
+void main(){
+    int Jumlah_Barang;
+    int Nama[50], Jenis[50], Harga[50];
+}
