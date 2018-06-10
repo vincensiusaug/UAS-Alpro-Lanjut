cc7a585b3e6f6dddecb555eeb45a07788318c756
Vincensius Augustino <39743055+vincensiusaug@users.noreply.github.com>
2018-06-10 17:11:27 +0700

    Update Restaurant.c

--- Restaurant.c
+++ Restaurant.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 
+
 int jumlah_barang = 50; 
+
 
 typedef struct barang
 {
@@ -51,38 +53,63 @@ void Data_Sort(){
 
 void Menu_Header(){
     puts ("====================================");
-    puts ("Mont Blanc Restaurant");
-    puts ("");
+    puts ("     Mont Blanc Restaurant");
     puts ("");
     puts ("====================================");
 }
 
 void Menu_Main(){
+    Menu_Header();
     puts ("1 - Tambah Barang");
     puts ("2 - Ubah Barang");
     puts ("3 - Urutkan Barang");
     puts ("4 - Pembelian");
+    puts ("Q - Keluar");
 
 }
 
 void Menu_Add(){
+    Menu_Header();
+    puts ("1 - Tambah Makanan");
+    puts ("2 - Tambah Minuman");
+    puts ("Q - Kembali");
+}
 
+int Makanan(){
+    char nama;
+    int harga;
+    printf ("Masukan nama makanan: ");
+    scanf ("%c",&nama);
+    printf ("Masukan harga makanan: ");
+    scanf ("%d",&harga);
+    return harga;
+}
+
+int Minuman(){
+    char nama[100];
+    int harga;
+    printf ("Masukan nama minuman: ");
+    scanf ("%c",&nama);
+    printf ("Masukan harga minuman: ");
+    scanf ("%d",&harga);
+    return harga;
 }
 
 void Menu_Edit(){
-
+    Menu_Header();
 }
 
 void Menu_Sort(){
-
+    Menu_Header();
 }
 
 void Menu_Purchase(){
-
+    Menu_Header();
 }
 
 void main(){
     barang nama[jumlah_barang];
     int jenis[jumlah_barang];
     int harga[jumlah_barang];
+    Menu_Main();
 }
